6fb6043e62718a753ac0c140031bc527ecd55fc6
Антон <103773083+kantosev@users.noreply.github.com>
2023-07-28 00:20:16 +0300

    Commit-2

---
+++ GitExample/GitExample/Example-1.swift
@@ -0,0 +1,18 @@
+//
+//  Example-1.swift
+//  GitExample
+//
+//  Created by Аркадий Варежкин on 28.07.2023.
+//
+
+import Foundation
+
+
+
+
+
+struct Person {
+    let name: String = "name"
+    let age: Int = 12
+    let description: String = "description"
+}
