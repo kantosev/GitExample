364dc3a6094567533635ecba6d33693c692f9a21
Антон <103773083+kantosev@users.noreply.github.com>
2023-07-28 00:16:59 +0300

    First Commit

---
+++ GitExample/GitExample/GitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExampleApp.swift
+//  GitExample
+//
+//  Created by Аркадий Варежкин on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
